a431ff73a82e96927d47f4fb72bee5f766dad5c4
hussainrefaa <hus244@gmail.com>
2016-03-15 09:26:24 +0100

    Update index.php

--- angularjs-crud-example_bootstrap/index.php
+++ angularjs-crud-example_bootstrap/index.php
@@ -20,76 +20,76 @@
  
     <!-- page content and controls will be here -->
     <div class="container" ng-app="myApp" ng-controller="employeesCtrl">
-        
-        <div class="row">
-            <div class="col s12">
-                <h4>Employees</h4>
-                <!-- used for searching the current list -->
-                <input type="text" ng-model="search" class="form-control" placeholder="Search employee...">
+         <!-- floating button for creating employee -->
+         <div class="fixed-action-btn" style="bottom:45px; right:24px;">
+             <a class="waves-effect waves-light btn modal-trigger btn-floating btn-large red" href="#modal-employee-form" ng-click="showCreateForm()"><i class="large material-icons">add</i>New Employee</a>
+         </div>
+         <div class="row">
+             <div class="col s12">
+                 <h4>Employees</h4>
+                 <!-- used for searching the current list -->
+                 <input type="text" ng-model="search" class="form-control" placeholder="Search employee...">
 
-                <!-- table that shows employee record list -->
-                <table class = "table table-striped">
-                    
-                    <thead>
-                        <tr>
-                            <th class="text-align-center">ID</th>
-                            <th class="width-30-pct">Name</th>
-                            <th class="width-30-pct">Dept</th>
-                            <th class="text-align-center">Salary</th>
-                            <th class="text-align-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody ng-init="getAll()">
-                        <tr ng-repeat="d in names | filter:search">
-                            <td class="text-align-center">{{ d.id }}</td>
-                            <td>{{ d.name }}</td>
-                            <td>{{ d.dept }}</td>
-                            <td class="text-align-center">{{ d.salary }}</td>
-                            <td>
-                                <a ng-click="readOne(d.id)" class="waves-effect waves-light btn margin-bottom-1em"><i class="material-icons left">edit</i>Edit</a>
-                                <a ng-click="deleteEmployee(d.id)" class="waves-effect waves-light btn margin-bottom-1em"><i class="material-icons left">delete</i>Delete</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <!-- floating button for creating employee -->
-                <div class="fixed-action-btn" style="bottom:45px; right:24px;">
-                    <a class="waves-effect waves-light btn modal-trigger btn-floating btn-large red" href="#modal-employee-form" ng-click="showCreateForm()"><i class="large material-icons">add</i></a>
-                </div>
+                 <!-- table that shows employee record list -->
+                 <table class = "table table-striped">
 
-                <!-- modal for for creating new employee -->
-                <div id="modal-employee-form" class="modal" >
-                    
-                    <div class="modal-content">
-                        <h4 id="modal-employee-title">Create New Employee</h4>
-                        
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Emp Name (Hussain Refaa)</span>
-                            <input id="form-name" class="validate" ng-model="name" type="text" class="form-control" placeholder="Type name here..." aria-describedby="basic-addon1">
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Dept (Maneger)</span>
-                            <input id="form-name" class="validate" ng-model="dept" type="text" class="form-control" placeholder="Type dept here..." aria-describedby="basic-addon1">
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1">Salary (0000.0000)</span>
-                            <input id="form-name" class="validate" id="form-salary" ng-model="salary" type="text" class="form-control" placeholder="Type salary here..." aria-describedby="basic-addon1">
-                        </div>
+                     <thead>
+                         <tr>
+                             <th class="text-align-center">ID</th>
+                             <th class="width-30-pct">Name</th>
+                             <th class="width-30-pct">Dept</th>
+                             <th class="text-align-center">Salary</th>
+                             <th class="text-align-center">Action</th>
+                         </tr>
+                     </thead>
+                     <tbody ng-init="getAll()">
+                         <tr ng-repeat="d in names | filter:search">
+                             <td class="text-align-center">{{ d.id }}</td>
+                             <td>{{ d.name }}</td>
+                             <td>{{ d.dept }}</td>
+                             <td class="text-align-center">{{ d.salary }}</td>
+                             <td>
+                                 <a ng-click="readOne(d.id)" class="waves-effect waves-light btn margin-bottom-1em"><i class="material-icons left">edit</i>Edit</a>
+                                 <a ng-click="deleteEmployee(d.id)" class="waves-effect waves-light btn margin-bottom-1em"><i class="material-icons left">delete</i>Delete</a>
+                             </td>
+                         </tr>
+                     </tbody>
+                 </table>
 
-                            
-                            <div class="modal-footer">
-                                <a id="btn-create-employee" class="btn btn-primary" ng-click="createEmployee()"><i class="material-icons left">add</i>Create</a>
 
-                                <a id="btn-update-employee" class="btn btn-primary" ng-click="updateEmployee()"><i class="material-icons left">edit</i>Save Changes</a>
+                 <!-- modal for for creating new employee -->
+                 <div id="modal-employee-form" class="modal" >
 
-                                <a class="btn btn-default" ng-click="closeDialog()"><i class="material-icons left">close</i>Close</a>
-                            </div>
-                        
-                    </div>
-                </div>
+                     <div class="modal-content">
+                         <h4 id="modal-employee-title">Create New Employee</h4>
 
-            </div> <!-- end col s12 -->
-        </div> <!-- end row -->
+                         <div class="input-group">
+                             <span class="input-group-addon" id="basic-addon1">Emp Name (Hussain Refaa)</span>
+                             <input id="form-name" class="validate" ng-model="name" type="text" class="form-control" placeholder="Type name here..." aria-describedby="basic-addon1">
+                         </div>
+                         <div class="input-group">
+                             <span class="input-group-addon" id="basic-addon1">Dept (Maneger)</span>
+                             <input id="form-name" class="validate" ng-model="dept" type="text" class="form-control" placeholder="Type dept here..." aria-describedby="basic-addon1">
+                         </div>
+                         <div class="input-group">
+                             <span class="input-group-addon" id="basic-addon1">Salary (0000.0000)</span>
+                             <input id="form-name" class="validate" id="form-salary" ng-model="salary" type="text" class="form-control" placeholder="Type salary here..." aria-describedby="basic-addon1">
+                         </div>
+
+
+                         <div class="modal-footer">
+                             <a id="btn-create-employee" class="btn btn-primary" ng-click="createEmployee()"><i class="material-icons left">add</i>Create</a>
+
+                             <a id="btn-update-employee" class="btn btn-primary" ng-click="updateEmployee()"><i class="material-icons left">edit</i>Save Changes</a>
+
+                             <a class="btn btn-default" ng-click="closeDialog()"><i class="material-icons left">close</i>Close</a>
+                         </div>
+
+                     </div>
+                 </div>
+
+             </div> <!-- end col s12 -->
+         </div> <!-- end row -->
     </div> <!-- end container -->
 
 <!-- include jquery -->
